34c9c7d5c9c89b26360acd5560873df72cf65184
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-02-06 23:32:38 +0300

    deploy backend
    scripts typo
    
    scripts typo2
    
    scripts typo4
    
    scripts typo5
    
    typo5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { errors } from "celebrate";
 // import cors from 'cors';
-import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
-import routes from './routes';
+import errorHandler from "./middlewares/error-handler";
+import { DB_ADDRESS } from "./config";
+import routes from "./routes";
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,6 +15,11 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,4 +28,4 @@ app.use(errors());
 app.use(errorHandler);
 
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log('ok'));
+app.listen(PORT, () => console.log("ok"));

d0fc5e81ddf7963cf11797261925e90842660381
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-02-07 15:58:19 +0300

    frontend deploy

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+const dontenv = require("dotenv");
+dontenv.config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF } =
+  process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto",
+      script: "dist/app.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
+    },
+  },
+};
